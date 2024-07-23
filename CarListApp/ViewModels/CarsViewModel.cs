58bfa15b829eb43186b7ea2d7fd24e0f5656732a
Gabor Benko <benkogabor87gmail.com>
2024-07-23 20:52:32 +0200

    carsviewmodel

---
+++ CarListApp/ViewModels/CarsViewModel.cs
@@ -0,0 +1,42 @@
+﻿using CarListApp.Models;
+using CarListApp.Services;
+using CommunityToolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+
+namespace CarListApp.ViewModels
+{
+    public partial class CarsViewModel : BaseViewModel
+    {
+        private readonly CarService carService;
+
+        public ObservableCollection<Car> Cars { get; private set; } = new ();
+
+        public CarsViewModel(CarService carService)
+        {
+            Title = "Car List";
+            this.carService = carService;
+        }
+
+        [RelayCommand]
+        async Task GetCars()
+        {
+            try
+            {
+                IsLoading = true;
+                if (IsLoading) return;
+                if (Cars.Any()) Cars.Clear();
+                carService.GetCars().ForEach(car => Cars.Add(car));
+            }
+            catch (Exception ex)
+            { 
+                Debug.WriteLine($"Unable to get cars: {ex.Message}");
+                await Shell.Current.DisplayAlert("Error", "Failed to retreive cars", "Ok");
+            }
+            finally
+            {
+                IsLoading = false;
+            }
+        }
+    }       
+}
